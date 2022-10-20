2eb87abd4f92f11a3525c4b50bbbbb56db8f7484
jossi <joepatriot30@gmail.com>
2022-10-20 12:24:59 +0300

    made changes to the script

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ const projects = [
     title: "Tonic",
     list: ["CANOPY", "Back End Dev", "2015"],
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+      "Lorem Ipsum is simply dummy text of the printings and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
     languages: ["HTML", "css", "Ruby and rails", "javascript"],
   },
   {
